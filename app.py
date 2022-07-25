d1c1d88cd15cfe965e83d21ebcabcfbeb23328c5
thienlongtran <ttran384@gatech.edu>
2022-07-25 17:40:13 -0500

    Started foundations

---
+++ app.py
@@ -0,0 +1,20 @@
+JFROG_URL = "https://thienlongtran.jfrog.io"
+
+class JFrog:
+    def __init__():
+        pass
+
+    def create_user():
+        pass
+
+    def delete_user():
+        pass
+
+    def create_repository():
+        pass
+
+    def update_repository():
+        pass
+    
+    def list_repositories():
+        pass
